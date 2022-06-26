5307770628d925f53343dbe67101c82052739438
Ruslan <style-26@mail.ru>
2022-06-26 23:40:32 +0400

    Add files via upload

---
+++ Versus/project.vue
@@ -0,0 +1,51 @@
+let app = new Vue({
+    el:'#app',
+    data: {
+            logo:'Versus',
+            intro__title:'We build beautiful web and mobile apps.',
+            works__title:'Latest Work',
+            footer__write:'Powered by Webflow',
+            link:'#',
+            btn: {
+                title: 'Get a quote',
+                IsDark: true, //or false
+            },
+            works: [{
+                id:'1',
+                img:'/images/Work-1.jpg',
+            },
+            {
+                id:'2',
+                img:'/images/Work-2.jpg',
+            },
+            {
+                id:'3',
+                img:'/images/Work-3.jpg',
+            },
+            {   
+                id:'4',
+                img:'/images/Work-4.jpg',
+            },
+            {   
+                id:'5',
+                img:'/images/Work-5.jpg',
+            },
+            {   
+                id:'6',
+                img:'/images/Work-6.jpg',
+            },
+            {   
+                id:'7',
+                img:'/images/Work-7.jpg',
+            },
+            {   
+                id:'8',
+                img:'/images/Work-8.jpg',
+            },
+            {   
+                id:'9',
+                img:'/images/Work-9.jpg',
+            },
+        ],
+    },
+})
